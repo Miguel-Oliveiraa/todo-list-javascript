6e9271bc0ac2691d635d587fc3d2bc54f09d5e96
migasrcf <miguellgomes64@gmail.com>
2022-04-08 23:15:30 -0300

    deleting and marking list itens with javascript

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-// Selectors
+/* Selectors */
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
-// Event Listeners
+/* Event Listeners */
 
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
-// Functions
+/* Functions */
 
 function addTodo(event) {
   // prevent form from submiting
   event.preventDefault();
-  console.log('migas');
 
   // todo Div
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
   // create li
   const newTodo = document.createElement('li');
-  newTodo.innerText = 'Migas';
-  newTodo.classList.add('item');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
   // Button check mark
   const completedButton = document.createElement('button');
@@ -30,8 +30,30 @@ function addTodo(event) {
   // Button delete
   const trashButton = document.createElement('button');
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add('complete-btn');
+  trashButton.classList.add('trash-btn');
   todoDiv.appendChild(trashButton);
   // append child
   todoList.appendChild(todoDiv);
+  // clear Todo INPUT VALUE
+  todoInput.value = '';
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+
+  // DELETE TODO
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    // animation
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function () {
+      todo.remove();
+    });
+  }
+
+  // CHECK MARK
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.add('completed');
+  }
 }
